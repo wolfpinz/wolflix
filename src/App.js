849d01a046c1cd3920e17dc9a62ee9e28fda6479
Wolf Pinz <wolfpinz@gmail.com>
2023-02-04 17:10:24 +0100

    added tailwind & react-icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline text-cyan-600">
+      auto save? nice!
+      <h3> Lets go for a <FaBeer />? </h3>
+    </h1>
     </div>
   );
 }
